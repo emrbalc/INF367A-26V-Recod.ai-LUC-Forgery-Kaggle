0f47bdb5594f6ced27dcaa7618d5c8398563870f
Emre Balci <emrbalc@gmail.com>
2026-03-06 15:51:30 +0100

    Implement SegNeXt backbone (MSCAN) using timm

--- models/segnext_segmenter.py
+++ models/segnext_segmenter.py
@@ -1,13 +1,50 @@
 import torch
 import torch.nn as nn
+import timm
+import torch.nn.functional as F
+
 
 class SegNeXtSegmenter(nn.Module):
+
     def __init__(self, out_ch: int = 1):
         super().__init__()
-        self.out_ch = out_ch
-        # Minimal learnable layer so optimizer has params
-        self.head = nn.Conv2d(3, out_ch, kernel_size=1)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # x: (B, 3, H, W)
-        return self.head(x)
+        # SegNeXt backbone (MSCAN)
+        self.backbone = timm.create_model(
+            "segnext_small",
+            pretrained=True,
+            features_only=True
+        )
+
+        channels = self.backbone.feature_info.channels()
+
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.Conv2d(channels[-1], 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(128, out_ch, 1)
+        )
+
+
+    def forward(self, x):
+
+        features = self.backbone(x)
+
+        x = features[-1]
+
+        x = self.decoder(x)
+
+        x = F.interpolate(
+            x,
+            scale_factor=32,
+            mode="bilinear",
+            align_corners=False
+        )
+
+        return x
